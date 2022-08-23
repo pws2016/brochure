928357715be4b2697ab8760d72c211bb05b7d247
sinda <mildsinda@gmail.com>
2022-08-23 11:29:31 +0100

    config

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/brochure/broch/public/';
+    public $baseURL = 'http://localhost/broch/brochure/public/';
 
     /**
      * --------------------------------------------------------------------------
